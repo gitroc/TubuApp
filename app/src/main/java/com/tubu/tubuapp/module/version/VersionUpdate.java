562e5321e4733bb6e84c5285df897c3be1a12a40
gitroc <rocday@163.com>
2016-07-09 16:08:09 +0800

    add files.

---
+++ app/src/main/java/com/tubu/tubuapp/module/version/VersionUpdate.java
@@ -0,0 +1,12 @@
+package com.tubu.tubuapp.module.version;
+
+/**
+ * @Description: 版本更新
+ * @Copyright: Copyright (c) 2016 chexiang.com. All right reserved.
+ * @Author: songjunpeng
+ * @Date: 2016/7/9 15:19
+ * @Modifier: songjunpeng
+ * @Update: 2016/7/9 15:19
+ */
+public class VersionUpdate {
+}
